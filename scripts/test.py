245ab529332e741970967ed9e4d21cbc9e4b555e
iwanhk <iwan@isotop.top>
2022-05-24 12:07:56 +0800

    update datatemplate

--- scripts/test.py
+++ scripts/test.py
@@ -12,7 +12,15 @@ def main():
             pass
 
         if active_network in TEST_NETWORKS:
-            pass
+            data = DataTemplate[-1]
+            with open('animation.svg', 'r') as f:
+                buffer = f.read()
+                compress_data = deflate(str.encode(buffer))
+                print(
+                    f"animation.svg ({len(buffer)}) compressed to {len(compress_data)}")
+                data.update(0, compress_data, len(buffer), addr(admin))
+
+            print(f"{len(data.get(0))} bytes uploaded to dataTemplate")
 
     except Exception:
         console.print_exception()
